7ab722928986b46d403d84e75a0cfd837fbe7387
krishnasingh0210 <krishnasingh.02oct@gmail.com>
2026-05-22 12:15:30 +0530

    Final1 commit

--- project1/day3.py
+++ project1/day3.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 # Function to run when button is clicked
 def say_hello():
-        label.config(text="Hello Krishna!", fg="white") #foreground color
+        label.config(text="Hello Krishna!", foreground="white") #(foreground color or fg) is used to change text color of label
 
 # Create window
 window = tk.Tk()
@@ -21,7 +21,7 @@ label = tk.Label(
     text="Welcome",
     font=("Arial", 20),
     bg=background_color,
-    fg="white"
+    foreground="white"
 )
 
 label.pack(pady=20)
@@ -31,7 +31,8 @@ button = tk.Button(
     window,
     text="Click Me",
     font=("Arial", 16),
-    background="red",
+    background="lightgrey",
+    foreground="black",
     command=say_hello
 )
 
